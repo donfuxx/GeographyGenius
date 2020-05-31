8cdec5e2c1aa6ab9ce78c2e978c09e1c558b4058
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2020-05-31 19:09:54 +0100

    root build.gradle to build.gradle.kts

---
+++ buildSrc/src/main/java/Deps.kt
@@ -0,0 +1,8 @@
+object Versions {
+    const val kotlin = "1.3.72"
+    const val gradle = "4.0.0"
+}
+object Deps {
+    const val kotlinStandardLibrary = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+    const val gradleTools = "com.android.tools.build:gradle:${Versions.gradle}"
+}
