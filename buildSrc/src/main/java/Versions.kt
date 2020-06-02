5e99f2683f79ee5b3919429de40211a45300d227
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2020-06-02 08:39:34 +0100

    check only for stable versions updates

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val appCompat = "1.1.0"
     const val constraintLayout = "1.1.3"
-    const val coreKtx = "1.2.0"
+    const val coreKtx = "1.3.0"
     const val espressoCore = "3.2.0"
     const val gradle = "4.0.0"
     const val gradleVersionsPlugin = "0.28.0"
