30a9cb7d63ed46c297dc05e504a94788dd0db73e
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2020-06-12 16:43:25 +0100

    add home activity

--- app/src/main/java/com/appham/geographygenius/MainActivity.kt
+++ app/src/main/java/com/appham/geographygenius/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.appham.geographygenius
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.appham.geographygenius.features.home.HomeActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, HomeActivity::class.java))
     }
 }
