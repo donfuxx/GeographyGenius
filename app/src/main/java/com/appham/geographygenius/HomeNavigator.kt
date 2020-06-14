a230194a3602417a74a430e8984c67aa2fd4a1db
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2020-06-14 13:34:43 +0100

    Revert "add NavigationRouter.kt and mockk dependency"
    This reverts commit 86e0364b

--- app/src/main/java/com/appham/geographygenius/HomeNavigator.kt
+++ app/src/main/java/com/appham/geographygenius/HomeNavigator.kt
@@ -1,6 +1,9 @@
 package com.appham.geographygenius
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.appham.geographygenius.features.game.GameActivity
 import com.appham.geographygenius.features.home.HomeNavigation
 import com.appham.geographygenius.features.home.HomeNavigationEvent
 import com.appham.geographygenius.features.home.HomeViewModel
@@ -8,22 +11,25 @@ import org.koin.dsl.module
 
 val homeNavigatorModule = module {
     single<HomeNavigation> { (activity: AppCompatActivity, homeViewModel: HomeViewModel) ->
-        HomeNavigator(NavigationRouter(activity), homeViewModel)
+        HomeNavigator(activity, homeViewModel)
     }
 }
 
 class HomeNavigator(
-    private val navigationRouter: NavigationRouter,
+    private val activity: AppCompatActivity,
     private val homeViewModel: HomeViewModel
 ) : HomeNavigation {
 
     override fun init() {
-
-        homeViewModel.getNavEvents().observe(navigationRouter)  { event ->
+        homeViewModel.getNavEvents().observe(activity, Observer { event ->
             when(event) {
-                is HomeNavigationEvent.GoToGame -> navigationRouter.goToGame()
+                is HomeNavigationEvent.GoToGame -> goToGame()
             }
-        }
+        })
+    }
+
+    private fun goToGame() {
+        activity.startActivity(Intent(activity, GameActivity::class.java))
     }
 
 }
