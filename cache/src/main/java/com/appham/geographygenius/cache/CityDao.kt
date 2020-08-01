4f8db13625e50b8de82ef7c093c6415210a2494e
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2020-08-01 14:50:11 +0100

    add insert city entity fun

--- cache/src/main/java/com/appham/geographygenius/cache/CityDao.kt
+++ cache/src/main/java/com/appham/geographygenius/cache/CityDao.kt
@@ -1,10 +1,15 @@
 package com.appham.geographygenius.cache
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 
 @Dao
 interface CityDao {
+
     @Query("SELECT * FROM cityentity")
     suspend fun getAll(): List<CityEntity>
+
+    @Insert
+    suspend fun insertAll(vararg users: CityEntity)
 }
